ba6c7f3ac4892ec1ff05a0569fd9b714b6a9896b
Alan Chung <alanchung2k2@gmail.com>
2025-05-07 01:05:17 -0400

    training data setup

--- preprocessor.py
+++ preprocessor.py
@@ -16,12 +16,20 @@ class Preprocessor:
 
   def __init__(self) -> None:
     self.stemmer = PorterStemmer()
-
+  
+  # split string into words/components (Ex. aren't -> are n't)
   def tokenize(self, phrase) -> list[str]:
     return nltk.word_tokenize(phrase)
 
+  # generate root form of words (Ex. organize -> organ)
   def stem(self, word):
     return self.stemmer.stem(word.lower())
 
+  # generate bag of words vector (all words vector with 1/0 to indicate the word's presence)
+  """
+  tokenizedPhrase =    ["hello", "how", "are", "you"]
+  allWords =           ["hi", "hello", "I", "you", "bye", "thank", "cool"]
+  bagOfWordsVector =   [  0 ,    1   ,  0 ,  1   ,   0  ,    0   ,   0   ]
+  """
   def bagOfWordsVector(self, tokenizedPhrase, allWords):
     pass
--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import json
+import numpy as np
 from preprocessor import Preprocessor 
 
 with open('intents.json', 'r') as file:
@@ -10,17 +11,26 @@ preprocessor = Preprocessor()
 allWords = []
 tags = []
 xy = []
+X_train = []
+y_train = []
 
+# preprocessing steps
 for intent in intents['intents']:
   tag = intent['tag']
-  tags.append(tag)
+  tags.append(tag) # add to tags
   for pattern in intent['patterns']:
-    w = preprocessor.tokenize(pattern)
-    allWords.extend(w)
-    xy.append((w, tag))
+    words = preprocessor.tokenize(pattern) # tokenize patterns
+    allWords.extend(words) # add to all words
+    xy.append((words, tag)) # x = w (tokenized patterns) -> y = tag
 
 allWords = [preprocessor.stem(word) for word in allWords if word not in toIgnore]
-allWords = sorted(set(allWords))
-tags = sorted(set(tags))
-print(allWords)
-print(tags)
+allWords = sorted(set(allWords)) # stemmed and sorted all words (removed duplicates)
+tags = sorted(set(tags)) # sorted tag labels (removed duplicates)
+
+# setup training data
+for (words, tag) in xy:
+  bagVector = preprocessor.bagOfWordsVector(words, allWords) # get bag of words vector
+  X_train.append(bagVector)
+  y_train.append(tags.index(tag)) # store the associated tag index (For CrossEntropyLoss)
+X_train = np.array(X_train)
+y_train = np.array(y_train)
